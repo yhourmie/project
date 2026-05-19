6c97d3ddbb82daa0a607a765804a74d1073742db
yhourmie <yhourmie@gmail.com>
2026-05-19 21:59:10 +0200

    new code

---
+++ SS.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+data = pd.read_excel('908D4730.xlsx')
+data.head
+data.info()
